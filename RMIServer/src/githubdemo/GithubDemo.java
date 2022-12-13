766897ef2f888c2f7b0774f97bb9d58ebe6c937f
reemf011 <85066659+reemf011@users.noreply.github.com>
2022-12-13 23:38:59 +0300

    re

--- RMIServer/src/githubdemo/GithubDemo.java
+++ RMIServer/src/githubdemo/GithubDemo.java
@@ -3,64 +3,76 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package githubdemo;
 
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+//import rmi.GradeInterface;
 
 /**
  *
- * @author reemf011
+ * 
  */
 public class GithubDemo {
 
-    /**
-     * @param args the command line arguments
-     */
-       static PrivateCar s1;
-       
-    public static void main(String[] args) throws RemoteException{
+    static Student s1;
+    static Student s2;
+
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException {
         
-        // TODO code application logic here
-     
-         Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        
+                   
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
         mongoLogger.setLevel(Level.SEVERE);
-     
+        
         //Calling the class for the database 
-        db DB = new db();
+        db db = new db();
         
-           Registry r = LocateRegistry.createRegistry(1099);
+        // Here we create our remote object
+        //GradeInterface g = new Grade();
         
-        System.out.println("Server is ready");
+        // An RMI Registry initialized on port 1099
+        Registry r = LocateRegistry.createRegistry(1099);
         
-        s1 = new PrivateCar (1, 12, "ffmd", "12jn", 2021,"red");
-       
+        // Our remote object g is binded to the name "grade"
+        //r.bind("grade", g);
         
-        DB.insertCustomer(s1);
+        // Outputs that the server is ready
+        System.out.println("The server is ready");
         
         
-         DB.close();
+        s1 = new Student ("Meriam", 123, "Meriam12@yahoo.com");
+        s2 = new Student ("Alaa", 1234, "Alaa19@yahoo.com");
         
-//             Customer onlineMedia = new Customer();
-//              Admin personalLoan = new Admin("Special Offer Discount", 12.5f, "Perecent");
-//              personalLoan.registerObserver(onlineMedia);
-//              personalLoan.setSpecialOffers(20f);
-//              personalLoan.setSpecialOffers(30f);
-//              personalLoan.setSpecialOffers(50f);
+        db.insertStudent(s1);
+        db.insertStudent(s2);
         
-    }
-   
+         db.close();
+        } 
+
+
+         
+    
+    
 }
 
-    
-    
 
+    
+    
+//  Customer onlineMedia = new Customer();
+////              Admin personalLoan = new Admin("Special Offer Discount", 12.5f, "Perecent");
+////              personalLoan.registerObserver(onlineMedia);
+////              personalLoan.setSpecialOffers(20f);
+////              personalLoan.setSpecialOffers(30f);
+////              personalLoan.setSpecialOffers(50f);
     
 
--- RMIServer/src/githubdemo/db.java
+++ RMIServer/src/githubdemo/db.java
@@ -1,63 +1,58 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package githubdemo;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import com.google.gson.Gson;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Updates;
-import java.util.ArrayList;
+import java.rmi.RemoteException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.bson.Document;
 
+/**
+ *
+ * @author meriam
+ */
 public class db {
-
-    public MongoClient mongoclient;
-      public MongoDatabase mongodatabase;
-    public  MongoCollection <Document>  mongocollection;
-       private Gson gson = new Gson();
-       
-        public db() 
+    public static MongoClient mongoClient;
+    
+   public static MongoDatabase database;
+    
+   MongoCollection<Document> collection1;
+  
+   public static Gson gson = new Gson();
+    
+   
+   public db() 
     {
         // Disables Mongo Logs
         Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
         mongoLogger.setLevel(Level.SEVERE);
 
         // Initialize
-        mongoclient = new MongoClient();
+        mongoClient = new MongoClient();
         // Database name
-        mongodatabase = mongoclient.getDatabase("Resv1"); 
+        database = mongoClient.getDatabase("School"); 
         // Collection for the doctor 
-        mongocollection = mongodatabase.getCollection("PrivateCar"); 
+        collection1 = database.getCollection("Student"); 
   
    
     }
-        
-    public void insertCustomer(PrivateCar s) {
-        mongocollection.insertOne(Document.parse(gson.toJson(s)));
-        System.out.println("PrivateCar inserted.");
+   
+         public void insertStudent(Student s) 
+    {
+        collection1.insertOne(Document.parse(gson.toJson(s)));
+        System.out.println("Student is inserted.");
     }
-    
-
-
-
-        public void close() {
-        mongoclient.close();
+         
+         
+         public void close() 
+    {
+        mongoClient.close();
     }
-    
 }
-
-    
-
-
-/**
- *
- * @author DeS
- */
