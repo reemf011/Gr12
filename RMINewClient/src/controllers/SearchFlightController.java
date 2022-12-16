85105fbbd61ed234e458f36ac18856d4bd0471b5
RowanDahroug591 <rown192823@bue.edu.eg>
2022-12-16 18:19:06 +0200

    rowan

---
+++ RMINewClient/src/controllers/SearchFlightController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import ALLGUI.SearchFlight;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+//import rmi.CourseFacadeInterface;
+//import rmi.CourseInterface;
+//import rmi.StaffInterface;
+
+/**
+ *
+ * @author note book
+ */
+public class SearchFlightController {
+     SearchFlight gui;
+    Registry r;
+    
+        public controllers(SearchFlight gui, Registry r) throws RemoteException, NotBoundException {
+
+}
+
+  
