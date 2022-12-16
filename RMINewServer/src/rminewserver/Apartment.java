2d62fa77f5440bc30fbdd9a3231874a50fc468fa
Farah Dorgham <farahwael01@gmail.com>
2022-12-16 10:13:45 +0200

    GUI

--- RMINewServer/src/rminewserver/Apartment.java
+++ RMINewServer/src/rminewserver/Apartment.java
@@ -11,19 +11,11 @@ package rminewserver;
  */
 public class Apartment {
     int Apartmentid;
-    String Apartmentname;
     String Owner;
     String start_date;
     String end_date;
     double A_price;
     String Location;
-    public String getApartmentname() {
-        return Apartmentname;
-    }
-
-    public void setApartmentname(String Apartmentname) {
-        this.Apartmentname = Apartmentname;
-    }
 
     public String getOwner() {
         return Owner;
@@ -32,5 +24,26 @@ public class Apartment {
     public void setOwner(String Owner) {
         this.Owner = Owner;
     }
+
+    public int getApartmentid() {
+        return Apartmentid;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public double getA_price() {
+        return A_price;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+    
     public void Book(){}
 }
