d6feac8bd19302ba951e95bdbecf3f3a33ae37e3
MostafaaAmin <118075288+MostafaaAmin@users.noreply.github.com>
2022-12-17 05:15:01 +0200

    MM

--- RMINewServer/src/rminewserver/FeedbackDetails.java
+++ RMINewServer/src/rminewserver/FeedbackDetails.java
@@ -5,16 +5,57 @@
  */
 package rminewserver;
 
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import rmi.FeedBackInterface;
+
 /**
  *
  * @author DeS
  */
-public class FeedbackDetails {
-    int rating;
-    String complaint;
+public class FeedbackDetails extends UnicastRemoteObject  implements FeedBackInterface {
+    
+   
+    private int CustomerId;
+    private String date;
+    private int Rate;
+    private String complaint;
+     FeedBackDataMapper FeedBackDataMapper;
 
-    public int getRating() {
-        return rating;
+  public FeedbackDetails(Database db) throws RemoteException{
+       FeedBackDataMapper = new FeedBackDataMapper(db);
+    }
+
+    public FeedbackDetails(int CustomerId, String date, int Rate, String complaint) {
+        this.CustomerId = CustomerId;
+        this.date = date;
+        this.Rate = Rate;
+        this.complaint = complaint;
+    }
+
+    public int getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(int CustomerId) {
+        this.CustomerId = CustomerId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getRate() {
+        return Rate;
+    }
+
+    public void setRate(int Rate) {
+        this.Rate = Rate;
     }
 
     public String getComplaint() {
@@ -24,5 +65,44 @@ public class FeedbackDetails {
     public void setComplaint(String complaint) {
         this.complaint = complaint;
     }
-    public void receiveFeedback(){}
+
+    public FeedBackDataMapper getFeedBackDataMapper() {
+        return FeedBackDataMapper;
+    }
+
+    public void setFeedBackDataMapper(FeedBackDataMapper FeedBackDataMapper) {
+        this.FeedBackDataMapper = FeedBackDataMapper;
+    }
+  
+  
+      
+
+    @Override
+    public boolean AddNewFeedBack( int CustomerId, String date, int Rate, String complaint) throws RemoteException{
+        
+        return FeedBackDataMapper.insertFeedback(CustomerId, date, Rate, complaint);
+    }
+
+
+    @Override
+    public boolean deleteFeedBack(String type, int id) throws RemoteException{
+        return FeedBackDataMapper.deleteFeedBack(type, id);
+    }
+
+    @Override
+    public boolean UpdateFeedBack(int CustomerId, String date, int Rate, String complaint) throws RemoteException{
+        return FeedBackDataMapper.updateFeedBack(  CustomerId, date, Rate, complaint);
+    }
+
+
+    @Override
+    public ArrayList<String> ViewAll(int Sid) throws RemoteException{
+        return FeedBackDataMapper.viewAll(Sid);
+    }
+
+    @Override
+    public String toString() {
+        return "FeedBack{" + "CustomerId=" + CustomerId + ", date=" + date + ", Rate=" + Rate + ", complaint=" + complaint + '}';
+    }
+
 }
